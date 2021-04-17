f6531293566303dde2ecc48bc74e6fdf6d702fb9
valentino1337 <80648308+valentino1337@users.noreply.github.com>
2021-04-17 18:53:57 +0200

    fixed retarded while loops

--- main.py
+++ main.py
@@ -5,11 +5,6 @@ clicks = int(input('How many clicks?: '))
 driver = webdriver.Chrome()
 driver.get("https://ples.click")
 
-clicked = 0
-
-while clicked < clicks:
+for ples in range(clicks):
     driver.find_element_by_id('#zalud-container').click()
-    clicked = clicked + 1
-while clicked == clicks:
-    print('Clicked ' + str(clicked) + ' times')
-    sys.exit()
+sys.exit()
