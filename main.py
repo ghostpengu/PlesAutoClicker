ea71f4036235916920824a51445cadba596f34c1
Pingu <77410713+ghostpengu@users.noreply.github.com>
2021-04-18 10:14:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,73 @@
 from selenium import webdriver
 import sys
 import chromedriver_autoinstaller
+import time
+
+print("""Welcome
+Selections:
+1.legit
+2.normal
+3.rage
+""")
 
 
-clicks = int(input('How many clicks?: '))
-chromedriver_autoinstaller.install() #install web driver
-driver = webdriver.Chrome()
-driver.get("https://ples.click")
+def normal():
+    clicks = int(input('How many clicks?: '))
+    chromedriver_autoinstaller.install()
+    driver = webdriver.Chrome()
+    driver.get("https://ples.click")
+    clicked = 0
+    
+    while clicked < clicks:
+        driver.find_element_by_id('#zalud-container').click()
+        clicked = clicked + 1
+        time.sleep(0.2)
+    while clicked == clicks:
+        print('Clicked ' + str(clicked) + ' times')
+        sys.exit()
 
-clicked = 0
+def legit():
+    clicks = int(input('How many clicks?: '))
+    chromedriver_autoinstaller.install()
+    driver = webdriver.Chrome()
+    driver.get("https://ples.click")
+    clicked = 0
+    
+    while clicked < clicks:
+        driver.find_element_by_id('#zalud-container').click()
+        clicked = clicked + 1
+        time.sleep(0.5)
+    while clicked == clicks:
+        print('Clicked ' + str(clicked) + ' times')
+        sys.exit()
+    
+    
 
-while clicked < clicks:
-    driver.find_element_by_id('#zalud-container').click()
-    clicked = clicked + 1
-while clicked == clicks:
-    print('Clicked ' + str(clicked) + ' times')
-    sys.exit()
+def rage():
+    clicks = int(input('How many clicks?: '))
+    chromedriver_autoinstaller.install()
+    driver = webdriver.Chrome()
+    driver.get("https://ples.click")
+    clicked = 0
+    
+    while clicked < clicks:
+        driver.find_element_by_id('#zalud-container').click()
+        clicked = clicked + 1
+        
+    while clicked == clicks:
+        print('Clicked ' + str(clicked) + ' times')
+        sys.exit()
+
+
+command = input("$ ")
+
+if(command == 'rage'):
+    rage()
+
+
+
+if(command == 'legit'):
+    legit()
+
+if(command == 'normal'):
+    normal()
